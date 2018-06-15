d16efea577a744dbd0c3a325fcb4f45371d5e617
JuliaKan <39783445+JuliaKan@users.noreply.github.com>
2018-06-15 15:48:38 -0400

    huge refactor

--- tictactoe.js
+++ tictactoe.js
@@ -1,9 +1,7 @@
-
 let turnCount = 0
 let game = {
     currentPlayer: 'X',
     board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-
 }
 console.log("Let's play Tic Tac Toe! Press Enter to play.")
 
@@ -18,10 +16,10 @@ process.stdin.once('data', (chunk) => {
 
 function printGameBoard(board) {
 
-    let firstLine = board[0] + ' | ' + board[1] + ' | ' + board[2]
-    let horizontalSpacer = "--- --- ---"
-    let secondLine = board[3] + ' | ' + board[4] + ' | ' + board[5]
-    let thirdLine = board[6] + ' | ' + board[7] + ' | ' + board[8]
+    let firstLine = " " + board[0] + ' | ' + board[1] + ' | ' + board[2]
+    let horizontalSpacer = "---|---|---"
+    let secondLine = " " + board[3] + ' | ' + board[4] + ' | ' + board[5]
+    let thirdLine = " " + board[6] + ' | ' + board[7] + ' | ' + board[8]
 
     console.log(firstLine)
     console.log(horizontalSpacer)
@@ -33,58 +31,82 @@ function printGameBoard(board) {
 function gameTurn() {
 
     process.stdin.once('data', (chunk) => {
+        
+        let playerChosenLocation = chunk.toString().trim()
+        let locationIndex = playerChosenLocation - 1
+        
+        
+        if (isEntryValid(playerChosenLocation, game.board)) {
 
-        let gameBoardLocation = (chunk.toString().trim()) - 1
- 
-        if (game.board[gameBoardLocation] === "X" || game.board[gameBoardLocation] === "O") {
-            console.log("Impossible! That cell is already full.")
-            return gameTurn()
-        }
-
-        let winHasHappened = detectWinState(game.board, game.currentPlayer)
-
-        if (winHasHappened) {
-            console.log("Congratulations! " + game.currentPlayer + " wins!")
-            process.exit()
-
-        } else {
-
-            game.board[gameBoardLocation] = game.currentPlayer
-
-            //decide how to change player
-            if (game.currentPlayer === "X") {
-                game.currentPlayer = "O"
+            let winHasHappened = detectWinState(game.board, game.currentPlayer)
+        
+            if (winHasHappened) {
+                console.log("Congratulations! " + game.currentPlayer + " wins!")
+                process.exit()
             }
-            else {
-                game.currentPlayer = 'X'
-                //return gameTurn()
-            }
-            //update game state
 
-            printGameBoard(game.board)
-
-            let newTurnCount = turnCount++
-            //console.log(newTurnCount)
-    
-            if (turnCount === 9) {
+            if (checkForDraw()) {
                 console.log("It's a draw.")
-                process.exit
+                process.exit()
+
             }
-            console.log("Ready Player " + game.currentPlayer)
-            console.log("Move to?")
+            
+            game.board[locationIndex] = game.currentPlayer
+
+            togglePlayer()
+            printGameBoard(game.board)
             gameTurn()
 
         }
 
+        else {
+            console.log("Invalid Input!")
+            gameTurn()
+
+        }
     })
 }
-//work in progress. does not console.log winner statement because of problem with current player, see next line.
+
+//checking if playerInput is a valid number
+function isEntryValid(value, board) {
+
+    if (board.indexOf(parseInt(value)) === -1) {
+        console.log("Please ONLY enter a number between 1 and 9.")
+        return false
+
+    } else {
+        console.log ("this is " + value)
+        if (inputAlreadyUsed(value)) {
+            console.log ("This space has already been used. Please pick another one.")
+            return false
+        }
+        else {
+            return true
+        }
+
+
+    }
+}
+
+//when winstate is achieved, it is somehow connected to this following function and says this space has already been used , invalid input.
+function inputAlreadyUsed(location) {
+    if (game.board[location] === "X" || game.board[location] === "O") {
+        return true
+    }
+
+    return false
+
+}
+
+
 function detectWinState(board, currentPlayer) {
+
+    //console.log('Debug: ' + board[0] + ' ' + board[1] + ' ' + board[2] + ' - - ' + currentPlayer)
 
     if (board[0] === currentPlayer && board[1] === currentPlayer && board[2] === currentPlayer) {
         return true
     }
-    if (board[3] === currentPlayer && board[4] ===currentPlayer && board[5] === currentPlayer) {
+    if (board[3] === currentPlayer && board[4] === currentPlayer && board[5] === currentPlayer) {
         return true
     }
     if (board[6] === board[7] && board[7] === board[8]) {
@@ -110,6 +132,22 @@ function detectWinState(board, currentPlayer) {
 
 }
 
+function checkForDraw() {
+    if (game.turnCount === 9) {
+        return true
+    }
+
+    return false
+}
+
+function togglePlayer(){
+    if (game.currentPlayer === "X") {
+        game.currentPlayer = "O"
+    }
+    else {
+        game.currentPlayer = 'X'
+    }
+}
 
 
 
